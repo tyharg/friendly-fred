ee24564aa019ff327d28c33710f39ec1874657bc
tyharg <tytom22@gmail.com>
2025-03-11 18:49:20 -0500

    articles controller boilerplate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,22 @@
 class ArticlesController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
+    def article_params
+        params.permit(:title, :content, :author, :category, :published_at)
+      end
+
 end
