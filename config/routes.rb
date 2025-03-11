aa90a7e0fab593a1389f993ee3cac5bcc5dc2e9b
tyharg <tytom22@gmail.com>
2025-03-11 18:42:47 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resources :articles, only: [:index, :create, :show] do
+    member do
+      delete :destroy, to: 'articles#destroy'
+      put :update, to: 'articles#update'
+      patch :update, to: 'articles#patch'
+    end
+  end
+
+
+
 end
