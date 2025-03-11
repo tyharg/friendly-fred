fee5508a80ed20bf4080d63e17c2a712852554f4
tyharg <tytom22@gmail.com>
2025-03-11 18:46:28 -0500

    add validation to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-
-    
+    validates :title, presence: true, length: { maximum: 40 }
+    validates :content, :author, :category, :published_at, presence: true
 end
