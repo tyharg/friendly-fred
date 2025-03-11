dafb92ce360a16683990cac9f003be4937e93ee0
tyharg <tytom22@gmail.com>
2025-03-11 18:56:44 -0500

    write out articles actions according to spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,41 @@
 class ArticlesController < ApplicationController
-
     def index
+      articles = Article.order(published_at: :desc)
+      render json: articles.as_json(except: [:created_at, :updated_at])
     end
-
     def create
-    end
-
-    def show
-    end
-
-    def destroy
-    end
-
-    def update
-    end
-
-    def article_params
-        params.permit(:title, :content, :author, :category, :published_at)
+      article = Article.new(article_params)
+      if article.save
+        render json: article.as_json(except: [:created_at, :updated_at]), status: :created
+      else
+        render json: article.errors, status: :unprocessable_entity
       end
-
-end
+    end
+  
+    def show
+      article = Article.find_by(id: params[:id])
+      if article
+        render json: article.as_json(except: [:created_at, :updated_at])
+      else
+        render json: { error: 'Article not found' }, status: :not_found
+      end
+    end
+  
+    def destroy
+      head :method_not_allowed
+    end
+  
+    def update
+      head :method_not_allowed
+    end
+  
+    def patch
+      head :method_not_allowed
+    end
+  
+    private
+    def article_params
+      params.permit(:title, :content, :author, :category, :published_at)
+    end
+  end
+  
